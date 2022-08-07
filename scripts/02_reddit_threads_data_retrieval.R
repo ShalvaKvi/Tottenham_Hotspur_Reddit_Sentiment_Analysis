aba9e624cabc64971c4d4658711e9a56e22ab6fa
ShalvaKvi <indium94@gmail.com>
2022-08-08 00:47:55 +0300

    scripts commit

--- scripts/02_reddit_threads_data_retrieval.R
+++ scripts/02_reddit_threads_data_retrieval.R
@@ -2,48 +2,104 @@ library(tidyverse)
 library(tidylog)
 library(RedditExtractoR)
 
-# Retreive Reddit Daily Discussion URL's ----
+results <- read_csv("./data/results.csv")
 
-# Extract thread information
-daily_thread_search <- RedditExtractoR::find_thread_urls(keywords = "Daily Discussion Thread",
-                                                         sort_by = "new", 
+# Retrieve Reddit r/coys Thread Information via the API----
+
+## Daily Discussions ----
+## Extract thread information
+daily_thread_search <- find_thread_urls(keywords = "Daily Discussion Thread",
                                                          subreddit = "coys",
-                                                         period = "all")
+                                                         sort_by = "new",
+                                                         period = "year")
 
 
-# Extract some more thread information from last year
-daily_thread_search_21 <- RedditExtractoR::find_thread_urls(keywords = "Daily Discussion Thread (2021)",
-                                                         sort_by = "new", 
+## Extract some more thread information from last year
+daily_thread_search_21 <- find_thread_urls(keywords = "Daily Discussion Thread (2021)",
                                                          subreddit = "coys",
-                                                         period = "all")
+                                                         sort_by = "new",
+                                                         period = "year")
 
-
-# Bind dataframes together
+## Bind dataframes together
 daily_threads_info <- daily_thread_search |> 
   bind_rows(daily_thread_search_21)
 
+## Filter out irrelevant results
 daily_threads_info <- daily_threads_info |> 
-  filter(date_utc > min(results$date) - 3 & date_utc < max(results$date) + 3) |> 
+  filter(date_utc > min(results$date) - lubridate::days(3) & date_utc < max(results$date) + lubridate::days(3)) |> 
   filter(str_extract(string = title, pattern = 'Daily Discussion Thread') == 'Daily Discussion Thread') |> 
-  distinct(title, .keep_all = TRUE) 
+  mutate(title = str_replace_all(title, pattern = "[:punct:]", replacement = "_")) |> 
+  distinct(title, .keep_all = TRUE) |> 
   arrange(date_utc)
 
-# Save
-write_csv(daily_threads_info, "./data/daily_threads_info.csv")
+## Save
+write_csv(daily_threads_info, "./data/thread_search_results/daily_threads_info.csv")
+
+
+## Pre-Match Threads ----
+pre_match_search <-  find_thread_urls(keywords = "Pre-Match Thread", 
+                                      subreddit = "coys", 
+                                      sort_by = "new",
+                                      period = "year")
+
+
+pre_match_info <- pre_match_search |> 
+  mutate(date_utc = as.POSIXct(date_utc, tz = "utc")) |> 
+  arrange(date_utc) |> 
+  filter(date_utc %in% (results$date - lubridate::days(1)) | date_utc %in% (results$date)) |> 
+  mutate(title = str_replace_all(title, pattern = "[:punct:]", replacement = "_"))
+
+write_csv(pre_match_info, "./data/thread_search_results/pre_match_threads_info.csv")
+
+## Match Threads ----
+match_search <-  find_thread_urls(keywords = "[Match Thread]", 
+                                  subreddit = "coys", 
+                                  sort_by = "new",
+                                  period = "all")
+
+
+match_info <- match_search |> 
+  mutate(date_utc = as.POSIXct(date_utc, tz = "utc")) |> 
+  filter(str_extract(string = title, pattern = '\\[Match Thread\\]') == '[Match Thread]') |> 
+  arrange(date_utc) |> 
+  filter(date_utc %in% (results$date)) |> 
+  mutate(title = str_replace_all(title, pattern = "[:punct:]", replacement = "_"))
+
+
+
+write_csv(match_info, "./data/thread_search_results/match_threads_info.csv")
+## Post-Match Threads ----
+post_match_search <-  find_thread_urls(keywords = "Post-Match Thread", 
+                                       subreddit = "coys", 
+                                       sort_by = "new",
+                                       period = "year")
+
+
+post_match_info <- post_match_search |> 
+  mutate(date_utc = as.POSIXct(date_utc, tz = "utc")) |> 
+  arrange(date_utc) |> 
+  filter(date_utc  %in% (results$date)) |> 
+  mutate(title = str_replace_all(title, pattern = "[:punct:]", replacement = "_"))
+
+write_csv(post_match_info, "./data/thread_search_results/post_match_threads_info.csv")
 
 
 # Download Daily Threads Content ----
 
 # Read CSVs
-daily_threads_info <- read_csv("./data/daily_threads_info.csv")
-results <- read_csv("./data/results.csv")
+daily_threads_info <- read_csv("./data/thread_search_results/daily_threads_info.csv")
+pre_match_info <- read_csv("./data/thread_search_results/pre_match_threads_info.csv")
+match_info <- read_csv("./data/thread_search_results/match_threads_info.csv")
+post_match_info <- read_csv("./data/thread_search_results/post_match_threads_info.csv")
 
 
 # Run a loop that extract the thread content via Reddit API and save the rds file, also track progression
+
+# for daily threads
 for (i in 1:length(daily_threads_info$url)) {
   
 thread <- RedditExtractoR::get_thread_content(daily_threads_info$url[i])
-file_name <- glue::glue('./data/threads/{filename}.rds', 
+file_name <- glue::glue('./data/threads/daily/{filename}.rds', 
                         filename = daily_threads_info$title[i])
 
 write_rds(thread, file = file_name)
@@ -51,3 +107,43 @@ write_rds(thread, file = file_name)
 print(paste('progress:', i, '/', length(daily_threads_info$url)))
 
 }
+
+# for pre match threads
+for (i in 1:length(pre_match_info$url)) {
+  
+  thread <- RedditExtractoR::get_thread_content(pre_match_info$url[i])
+  file_name <- glue::glue('./data/threads/pre_match/{filename}.rds', 
+                          filename = pre_match_info$title[i])
+  
+  write_rds(thread, file = file_name)
+  
+  print(paste('progress:', i, '/', length(pre_match_info$url)))
+  
+}
+
+# for match threads
+for (i in 1:length(match_info$url)) {
+  
+  thread <- RedditExtractoR::get_thread_content(match_info$url[i])
+  file_name <- glue::glue('./data/threads/match/{filename}.rds', 
+                          filename = match_info$title[i])
+  
+  write_rds(thread, file = file_name)
+  
+  print(paste('progress:', i, '/', length(match_info$url)))
+  
+}
+
+# for post match threads
+for (i in 1:length(post_match_info$url)) {
+  
+  thread <- RedditExtractoR::get_thread_content(post_match_info$url[i])
+  file_name <- glue::glue('./data/threads/post_match/{filename}.rds', 
+                          filename = post_match_info$title[i])
+  
+  write_rds(thread, file = file_name)
+  
+  print(paste('progress:', i, '/', length(post_match_info$url)))
+  
+}
+
--- scripts/03_sentiment_analysis.R
+++ scripts/03_sentiment_analysis.R
@@ -2,15 +2,24 @@ library(tidyverse)
 library(tidylog)
 library(sentimentr)
 
+
 # Getting everything ready ----
+## Read thread information
+daily_threads_info <- read_csv("./data/thread_search_results/daily_threads_info.csv")
+pre_match_info <- read_csv("./data/thread_search_results/pre_match_threads_info.csv")
+match_info <- read_csv("./data/thread_search_results/match_threads_info.csv")
+post_match_info <- read_csv("./data/thread_search_results/post_match_threads_info.csv")
+
 
 ## Read threads, then bind them into a nested dataframe using 'purrr::map_dfr'
-threads <- fs::dir_ls(path = './data/threads', glob = '*rds') |> 
-  purrr::map_dfr(read_rds)
+thread_urls <- c("./data/threads/daily/", "./data/threads/match/", "./data/threads/pre_match/", "./data/threads/post_match/")
+threads <- fs::dir_ls(path = thread_urls , glob = '*rds') |> 
+  purrr::map_dfr(read_rds) |> 
+  bind_cols()
 
 ## Split the nested dataframe
 comments <- threads$comments
-threads <- threads$threads |> distinct(date, .keep_all = TRUE)
+thread_info <- threads$threads |> distinct(title, .keep_all = TRUE)
 
 
 # Analysis ----
@@ -22,32 +31,52 @@ comments <- comments |>
 
 ## Convert to a "get_sentences" object from "sentimentr"
 sentences <- sentimentr::get_sentences(comments$comment)
+
 # write_rds(sentences, file = './data/sentences.rds') # intermediate save
 
 ## Compute the sentiment via the "sentiment" function
-sentiment_sentence <- sentimentr::sentiment(text.var = sentences)
+sentiment_sentence <- sentimentr::sentiment(text.var = sentences) |> 
+  as_tibble() |> 
+  mutate(category = case_when(
+    sentiment < 0 ~ -1, 
+    sentiment == 0 ~ 0, 
+    sentiment > 0 ~ 1)
+  )
 # write_rds(sentiment_sentence, file = './data/sentiment_sentence.rds') # intermediate save
 
 # Aggregate sentiment on the comment and thread levels
-## Comment level
-sentiment_comment <- sentiment |> 
+## Comment level -
+sentiment_comment <- sentiment_sentence |> 
   group_by(element_id) |> 
-  summarise(mean_sentiment = mean(sentiment, na.rm = TRUE),
+  summarise(mean_sentiment = mean(category, na.rm = TRUE),
             mean_words = mean(word_count, na.rm = TRUE))
 
+### Bind the sentiment score the the comment dataframe
 comments <- comments |> 
 bind_cols(sentiment_comment)  
   
-## Thread level
+## Thread level - 
 sentiment_thread <- comments |> 
   group_by(url) |> 
   summarise(polarity = sd(mean_sentiment, na.rm = TRUE),
             mean_sentiment = mean(mean_sentiment, na.rm = TRUE),
             mean_words = mean(mean_words, na.rm = TRUE))
 
-threads <- threads |> 
+### Join the thread sentiment score to the thread_info dataframe
+thread_info <- thread_info |> 
   left_join(sentiment_thread, by = c('url'))
 
-## Write files 
+### Create a column that indicates the thread type by matching the url for the search results
+thread_info <- thread_info |> 
+  mutate(thread_type = case_when(
+    url %in% pre_match_info$url ~ "pre_match",
+    url %in% match_info$url ~ "match",
+    url %in% post_match_info$url ~ "post_match",
+    url %in% daily_threads_info$url ~ "daily",
+  ))
+
+# 
+
+## Write files -
 write_csv(comments, './data/comments.csv')
-write_csv(threads, './data/threads.csv')
+write_csv(thread_info, './data/thread_info.csv')
